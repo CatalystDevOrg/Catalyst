50a88d037464d80e60da2304e4acef68701897dc
JaydenDev <willsjayden2@gmail.com>
2021-12-19 13:28:37 -0600

    k

--- src/loader.js
+++ src/loader.js
@@ -2,3 +2,17 @@ function loadWeb() {
     var page = document.getElementById("searchbar").value;
     document.getElementById("view").src = page;
 }
+
+var webview = document.getElementById("view");
+
+function reload() {
+  view.reload();
+}
+
+function forward() {
+  view.goForward();
+}
+
+function backward() {
+  view.goBack();
+}
