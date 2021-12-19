9dfb991e6ca57f1184546227ab4007e4344bdc5e
JaydenDev <willsjayden2@gmail.com>
2021-12-19 13:53:46 -0600

    more stuff

--- main/main.js
+++ main/main.js
@@ -5,8 +5,8 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       webviewTag: true
