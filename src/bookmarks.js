4b4fb28e20e183ed4239eb21e2151541c522a557
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-12-25 19:28:02 +1300

    feat: add ability to add and remove bookmarks

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -1,17 +1,11 @@
 // initialise global variables
 const bookmarksBar = document.getElementById("bookmarks");
-
 // get bookmarks initialised
 if (window.localStorage.getItem("bookmarks") == null) {
-  window.localStorage.setItem("bookmarks", JSON.stringify([]));
+	window.localStorage.setItem("bookmarks", JSON.stringify([]));
 }
-
-for (let bookmarkIdx in JSON.parse(window.localStorage.getItem("bookmarks"))) {
-  const bookmark = JSON.parse(window.localStorage.getItem("bookmarks"))[bookmarkIdx]
-  addBookmarkToBar(bookmark.url, bookmark.title);
-  console.log(bookmark)
-}
-
+let bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
+updateBookmarksVar();
 // functions
 
 /**
@@ -20,14 +14,47 @@ for (let bookmarkIdx in JSON.parse(window.localStorage.getItem("bookmarks"))) {
  * @param {string} title The title of the page.
  */
 function addBookmarkToBar(url, title) {
-  let bookmarkEl = document.createElement("div");
-  bookmarkEl.innerText = title;
-  bookmarkEl.onclick = () => {
-    console.log("clicked on bookmark");
-    document.getElementById("searchbar").value = url;
-    console.log(document.getElementById("searchbar").value)
-    loadURL();
+	let bookmarkEl = document.createElement("div");
+	bookmarkEl.innerText = title;
+	bookmarkEl.onclick = () => {
+		document.getElementById("searchbar").value = url;
+		console.log(document.getElementById("searchbar").value);
+		loadURL();
+	};
+	bookmarkEl.classList.add("bookmark");
+	bookmarksBar.appendChild(bookmarkEl);
+}
+
+function bookmarkCurrentTab() {
+	updateBookmarksVar();
+  const toPush = { title: document.querySelector(".current").getTitle(), url: document.querySelector(".current").getURL() };
+  if (bookmarks.some(key => key.url === toPush.url)) {
+    if (confirm("Would you like to un-bookmark this page?")) {
+      for (let index in bookmarks) {
+        if (bookmarks[index].url == toPush.url) {
+          bookmarks.splice(index, 1);
+          break;
+        }
+      }
+      storeBookmarks();
+    };
+    return;
   };
-  bookmarkEl.classList.add("bookmark");
-  bookmarksBar.appendChild(bookmarkEl);
+	bookmarks.push(toPush);
+  storeBookmarks();
+}
+
+function updateBookmarksVar() {
+  console.log(window.localStorage.getItem("bookmarks"))
+	bookmarks = JSON.parse(window.localStorage.getItem("bookmarks"));
+	removeChildren(bookmarksBar);
+	for (let bookmarkIdx in bookmarks) {
+		const bookmark = bookmarks[bookmarkIdx];
+		addBookmarkToBar(bookmark.url, bookmark.title);
+	}
+}
+
+function storeBookmarks() {
+	window.localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  updateBookmarksVar();
 }
