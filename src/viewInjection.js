bc234a2447fe8c45a0025b52f36f6c92679d876e
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-04 13:10:44 +1300

    add base view injection

---
+++ src/viewInjection.js
@@ -0,0 +1,19 @@
+// TODO: allow users to store and use userstyles
+const currentView = () => document.querySelector(".current");
+let userstyles = [];
+if (window.localStorage.getItem("userstyles") == null) {
+	window.localStorage.setItem("userstyles", JSON.stringify([]));
+}
+
+// https://stackoverflow.com/questions/52143451/javascript-filter-with-wildcard
+const filterBy = (str, items) =>
+	items.filter((item) =>
+		new RegExp("^" + str.replace(/\*/g, ".*") + "$").test(item)
+	);
+
+currentView().addEventListener("did-start-loading", () => {
+  const filtered = filterBy(filterByuserstyles[index].matchURL, userstyles)
+	for (let index in filtered) {
+    // TODO: inject CSS
+  }
+});
