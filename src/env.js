2633512ee4551d59ea33b27c41cec0289bfafe72
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2022-01-13 18:18:11 -0600

    Create env.js

---
+++ src/env.js
@@ -0,0 +1,36 @@
+// This file contains variables, browser information, and functions to get this data
+// Similar to the navigator functions in all browsers
+
+// Security Patches
+securityPatchLevel = "January22Q1";
+securityPatchID = "1000268";
+latestSecurityPatch = "release11-patch001";
+currentSecurityPatch = "release11-patch001";
+
+// Browser Meta Data
+browserName = "Catalyst";
+browserEngine = "Chromium 94, Blink, Electron Latest";
+browserRepository = "https://github.com/JaydenDev/Catalyst";
+browserLicense = "MIT";
+
+// Functions to access data without entering file or preferences
+ function navBrowserName() {
+   console.log(browserName);
+ }
+
+ function navBrowserEngine() {
+   console.log(browserEngine);
+ }
+
+ function navBrowserRepository() {
+   console.log(browserRepository);
+ }
+
+ function navBrowserLicense() {
+   console.log(browserLicense);
+ }
+
+ function logSecurityPatchID() {
+   console.log(securityPatchID);
+   console.log(currentSecurityPatch);
+ }
