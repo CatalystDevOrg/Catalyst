c284819a402b9646c42854fa67fa7d8c21e43f74
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2022-03-14 14:23:56 -0500

    Create detection.js

---
+++ src/detection.js
@@ -0,0 +1,15 @@
+function checkEncryption() {
+    var page = document.getElementsByClassName("current").src;
+    if (page.includes("https://")) {
+        encrypted = true;
+    }
+    else {
+        encrypted = false;
+    }
+}
+value = "true"
+while (value = "true") {
+    checkEncryption()
+    print(encrypted)
+    print(page)
+}
