721590676bd231a35c88223df3d03a4902eef358
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-03-24 09:54:23 -0700

    Update pluginWorker.js

--- src/pluginWorker.js
+++ src/pluginWorker.js
@@ -1,9 +1,11 @@
 function loadPlugs() {
     // add a script tag to html to load every script in plugins variable
     for (let i = 0; i < plugins.length; i++) {
-        let script = document.createElement('script');
-        script.src = "../plugins/" + plugins[i] + "/plugin.js";
-        document.body.appendChild(script);
+        if (checkPlugin(plugins[i])) { 
+            let script = document.createElement('script');
+            script.src = "../plugins/" + plugins[i] + "/plugin.js";
+            document.body.appendChild(script);
+        }
     }
 }
 
