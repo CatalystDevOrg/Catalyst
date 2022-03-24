f2aed44cdf9a6929b0c3f688da3050a9b858d0fb
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-03-24 09:50:39 -0700

    Update pluginCheck.js

--- src/pluginCheck.js
+++ src/pluginCheck.js
@@ -1,5 +1,12 @@
-// Script to check if the script uses disallowed methods and throws error if it does
+// Script to check if the script uses disallowed methods and throws error if it does.
 
 function checkPlugin(namespace) {
-  let content = fetch().then(res => res.text)
+  let content = fetch("../plugins/" + namespace + "/plugin.js").then(res => res.text);
+  function testCondition(condition, errcod) {
+    if (condition) {
+      console.err(`Could not load plugin ${namespace} because of error: ${errcod}`)
+      return false;
+    }
+  }
+  [content.test(/[\0\s\n]process./), content.test(/[^"'`].*eval(/)].forEach(function(a) {a => testCondition(a, "Contains disallowed object")}) // This line disallows process and eval
 }
