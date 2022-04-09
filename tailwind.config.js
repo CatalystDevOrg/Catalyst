2e00d6faf5debb2e1022c27fec780eebdff839c3
JaydenDev <willsjayden2@gmail.com>
2022-04-09 13:27:34 -0500

    add UI animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	darkMode: "class",
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+    content: ["./src/**/*.{html,js}"],
+    darkMode: "class",
+    extend: {
+        // that is actual animation
+        keyframes: theme => ({
+            fadeOut: {
+                '0%': { backgroundColor: theme('colors.red.300') },
+                '100%': { backgroundColor: theme('colors.transparent') },
+            },
+        }),
+    }
+}
