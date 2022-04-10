058bcf50477314281fcffa4d4710391787026ba5
JaydenDev <willsjayden2@gmail.com>
2022-04-10 12:18:22 -0500

    init

--- src/listeners.js
+++ src/listeners.js
@@ -1,8 +1,3 @@
 document.getElementById("more-btn").addEventListener("click", () => {
     document.getElementById("more-menu").classList.toggle("hidden");
-    // add 'animate-bounce' class for 2 seconds
-    document.getElementById("more-menu").classList.add("animate-bounce");
-    setTimeout(() => {
-        document.getElementById("more-menu").classList.remove("animate-bounce");
-    }, 550);
 });
