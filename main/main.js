1f71acef846c54725de7cabbe0f7aa5730c4741a
JaydenDev <willsjayden2@gmail.com>
2022-10-14 10:31:14 -0500

    fix: disable update indicator

--- main/main.js
+++ main/main.js
@@ -1,16 +1,13 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, dialog } = require("electron");
 const path = require("path");
 const { Menu } = require("electron");
 const { fetch } = require("cross-fetch");
 
-// fix bug where application runs during installation
 if (require("electron-squirrel-startup")) app.quit();
 
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 768,
@@ -23,36 +20,19 @@ function createWindow() {
         title: "Catalyst",
         icon: path.join(__dirname, "../assets/icon.png"),
     });
-    // and load the index.html of the app.
     mainWindow.loadFile("./src/index.html");
     mainWindow.setMenuBarVisibility(false)
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
-
-    // update checking will be temporarily disabled until further notice.
-    checkForUpdate(mainWindow);
-
-    // setup auto update
     require('update-electron-app')()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     createWindow();
 
     app.on("activate", function() {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", function() {
     if (process.platform !== "darwin") app.quit();
 });
@@ -64,20 +44,17 @@ app.on("web-contents-created", function(event, contents) {
     }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
 try {
     require("electron-reloader")(module);
 } catch {}
 
+/*
 async function checkForUpdate(windowToDialog) {
     try {
         const githubFetch = await fetch(
             "https://api.github.com/repos/JaydenDev/Catalyst/releases"
         );
         if (!githubFetch.ok) {
-            // this means that
             alert("There was an error checking for a new update, check your WiFi connection and try again from the menubar.");
             return;
         }
@@ -105,6 +82,7 @@ async function checkForUpdate(windowToDialog) {
         console.error(error);
     }
 }
+*/
 
 let ver = app.getVersion();
 let appName = app.getName();
@@ -148,17 +126,16 @@ const template = [{
         label: "DevTools",
         accelerator: "CmdOrCtrl+I",
         click: function() {
-            // toggle devTools
             mainWindow.webContents.toggleDevTools();
         }
-    },
+    }/*
     {
         label: "Check for Updates",
         accelerator: "CmdOrCtrl+U",
         click: function() {
             checkForUpdate(mainWindow);
         }
-    }
+    }*/
 ];
 
 
