59c7a1b94b155d3bc92ad8f69d364a9956859a47
zani <zxnimh@gmail.com>
2023-03-05 20:12:16 -0700

    refactor: delete things we don't need

--- main/preload.js
+++
@@ -1,8 +0,0 @@
-const { remote } = require('electron');
-const path = require('path');
-
-const dat = require(path.join(__dirname, '../package.json'),);
-
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#ver').innerText = 'v' + dat.version;
-});
