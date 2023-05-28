9d1dac9d9c0b613fa7ca33008e8c44f36f59a72d
jdev082 <willsjayden2@gmail.com>
2023-05-28 16:05:42 -0500

    fix: fix scaling issue with long version name

--- main/preload.js
+++ main/preload.js
@@ -4,5 +4,6 @@ const path = require('path');
 const dat = require(path.join(__dirname, '../package.json'),);
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#ver').innerText = 'v' + dat.version;
+    version = dat.version.substring(0, 5)
+    document.querySelector('#ver').innerText = 'v' + version;
 });
