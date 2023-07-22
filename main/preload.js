e9c0c5bcac6c93d354e9bebe87352591567a1d76
jdev082 <jdev082@jdev.eu.org>
2023-07-22 15:29:18 -0500

    feat: display version in preferences

--- main/preload.js
+++ main/preload.js
@@ -4,5 +4,6 @@ const path = require('path');
 const dat = require(path.join(__dirname, '../package.json'),);
 
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#ver').innerText = 'v' + dat.version;
+    document.getElementById('ver').innerText = 'v' + dat.version;
+    document.getElementById('pref-ver').innerText = 'v' + dat.version;
 });
