2ab709d553ae545c4e3b4d2b88be446841dfe8d3
jdev082 <jdev082@jdev.eu.org>
2023-07-24 14:16:41 -0500

    fix: fix titlebar

--- main/preload.js
+++ main/preload.js
@@ -6,7 +6,5 @@ const dat = require(path.join(__dirname, '../package.json'),);
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('ver').innerText = 'v' + dat.version;
     document.getElementById('pref-ver').innerText = 'v' + dat.version;
-    new Titlebar({
-        backgroundColor: 'white'
-    });
+    new Titlebar();
 });
