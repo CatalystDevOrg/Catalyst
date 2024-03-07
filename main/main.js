44127df2ddb3153780aea939bfa64d8a5462bda4
jdev082 <jdev082@jdev.eu.org>
2024-03-07 17:38:33 -0600

    chore: remove retired update code

--- main/main.js
+++ main/main.js
@@ -53,42 +53,6 @@ try {
     require('electron-reloader')(module);
 } catch { }
 
-/*
-async function checkForUpdate(windowToDialog) {
-    try {
-        const githubFetch = await fetch(
-            "https://api.github.com/repos/JaydenDev/Catalyst/releases"
-        );
-        if (!githubFetch.ok) {
-            alert("There was an error checking for a new update, check your WiFi connection and try again from the menubar.");
-            return;
-        }
-        const releaseJSON = await githubFetch.json();
-        const replacerRegex = /["."]/gm;
-        const appVersionStr = app.getVersion();
-        const tagVersionInt = Number(appVersionStr.replace(replacerRegex, ""));
-        for (let i in releaseJSON) {
-            const release = releaseJSON[i];
-            if (release.draft || release.prerelease) continue;
-            const replaced = release["tag_name"].replace(replacerRegex, "");
-            if (
-                tagVersionInt <
-                Number(replaced.startsWith("v") ? replaced.slice(1) : replaced)
-            ) {
-                dialog.showMessageBox(windowToDialog, {
-                    message: "An update is available for Catalyst.",
-                    detail: `Go to github.com/JaydenDev/Catalyst/releases to install Catalyst ${release["tag_name"]}`,
-                    type: "info",
-                });
-                return;
-            }
-        }
-    } catch (error) {
-        console.error(error);
-    }
-}
-*/
-
 let ver = app.getVersion();
 let appName = app.getName();
 
