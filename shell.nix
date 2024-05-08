c9629e8722850cf0d7d9a9ca8901c7134d289a89
jdev082 <jdev082@jdev.eu.org>
2024-05-08 20:58:05 +0000

    feat(nix): development environment for nix
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSUserEnv {
+  name = "catalyst-devel";
+  targetPkgs = pkgs: (with pkgs;
+    [
+      nodejs python3 libcxx systemd libpulseaudio libdrm mesa stdenv.cc.cc
+      alsa-lib atk at-spi2-atk at-spi2-core cairo cups dbus expat fontconfig
+      freetype gdk-pixbuf glib gtk3 libnotify libuuid nspr nss pango systemd
+      libappindicator-gtk3 libdbusmenu libxkbcommon zlib
+    ]
+  ) ++ (with pkgs.xorg;
+    [
+      libXScrnSaver libXrender libXcursor libXdamage libXext libXfixes libXi
+      libXrandr libX11 libXcomposite libxshmfence libXtst libxcb
+    ]
+  );
+}).env
