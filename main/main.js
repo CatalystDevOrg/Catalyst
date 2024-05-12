686398e12f22ae820e516e4565f2c9640d3b5e88
jdev082 <jdev082@jdev.eu.org>
2024-05-12 14:17:56 -0500

    feat: split menu declaration into it's own file
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- main/main.js
+++ main/main.js
@@ -5,6 +5,7 @@ const { app, BrowserWindow, dialog, Menu, session, ipcMain, electron } = require
 const path = require('path');
 const fs = require('fs');
 const fetch = require('cross-fetch');
+const template = require('./menu.js');
 const contextMenu = require('electron-context-menu');
 
 if (require('electron-squirrel-startup')) app.quit();
@@ -72,63 +73,6 @@ function createAboutWindow() {
     aboutWindow.setMenuBarVisibility(false);
 
 }
-
-const template = [{
-    label: 'About',
-    click: function () {
-        createAboutWindow();
-    }
-},
-{
-    label: 'Quit',
-    click: function () {
-        app.quit();
-    }
-},
-{
-    label: 'New Tab',
-    accelerator: 'CmdOrCtrl+T',
-    click: function () {
-        mainWindow.webContents.executeJavaScript('createTab()');
-    }
-},
-{
-    label: 'Close Tab',
-    accelerator: 'CmdOrCtrl+W',
-    click: function () {
-        mainWindow.webContents.executeJavaScript('removeTab()');
-    }
-},
-{
-    label: 'Fullscreen',
-    accelerator: 'F11',
-    click: function () {
-        mainWindow.webContents.executeJavaScript('toggleFullScreen()');
-    }
-},
-{
-    label: 'Find',
-    accelerator: 'CmdOrCtrl+F',
-    click: function () {
-        mainWindow.webContents.executeJavaScript('toggleFind()');
-    }
-},
-{
-    label: 'DevTools',
-    accelerator: 'CmdOrCtrl+I',
-    click: function () {
-        mainWindow.webContents.toggleDevTools();
-    },
-
-}/*
-    {
-        label: "Check for Updates",
-        accelerator: "CmdOrCtrl+U",
-        click: function() {
-            checkForUpdate(mainWindow);
-        }
-    }*/
-];
 
 app.on('web-contents-created', (e, contents) => {
     contextMenu({
