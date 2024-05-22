f3e2092b1a9b4bdd231c3167f3ade3c0f9c18905
jdev082 <jdev082@jdev.eu.org>
2024-05-22 14:13:03 -0500

    feat(nix): add node-check-updates to nix shell
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@
   name = "catalyst-devel";
   targetPkgs = pkgs: (with pkgs;
     [
-      nodejs python3 libcxx systemd libpulseaudio libdrm mesa stdenv.cc.cc
+      nodejs npm-check-updates python3 libcxx systemd libpulseaudio libdrm mesa stdenv.cc.cc
       alsa-lib atk at-spi2-atk at-spi2-core cairo cups dbus expat fontconfig
       freetype gdk-pixbuf glib gtk3 libnotify libuuid nspr nss pango systemd
       libappindicator-gtk3 libdbusmenu libxkbcommon zlib
