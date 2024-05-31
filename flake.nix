dbf02911a34f0cf17347f2ab7dd5aa6c96ead91e
jdev082 <jdev082@jdev.eu.org>
2024-05-31 17:14:19 -0500

    feat(nix): shell -> develop/direnv
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 {
-  description = "Catalyst Browser Flake";
+  description = "A Nix-flake-based Node.js development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.catalyst3 = nixpkgs.legacyPackages.x86_64-linux.callPackage ./package.nix { };
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.catalyst3;
-  };
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev: rec {
+          nodejs = prev.nodejs_latest;
+          pnpm = prev.nodePackages.pnpm;
+          yarn = (prev.yarn.override { inherit nodejs; });
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ nodejs pnpm yarn ];
+        };
+      });
+    };
 }
