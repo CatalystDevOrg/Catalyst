0014c98a34beb717326fe97e9be9bbea41888740
jdev082 <jdev082@jdev.eu.org>
2024-10-20 12:43:48 -0500

    feat(nix): improved nix devshell for development
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
         (final: prev: rec {
           nodejs = prev.nodejs_latest;
           pnpm = prev.nodePackages.pnpm;
-          yarn = (prev.yarn.override { inherit nodejs; });
         })
       ];
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -20,7 +19,19 @@
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ nodejs pnpm yarn ];
+          packages = (with pkgs;
+            [
+              nodejs python3 libcxx systemd libpulseaudio libdrm mesa stdenv.cc.cc
+              alsa-lib atk at-spi2-atk at-spi2-core cairo cups dbus expat fontconfig
+              freetype gdk-pixbuf glib glibc gtk3 libnotify libuuid nspr nss pango systemd
+              libappindicator-gtk3 libdbusmenu libxkbcommon zlib
+            ]
+          ) ++ (with pkgs.xorg;
+          [
+            libXScrnSaver libXrender libXcursor libXdamage libXext libXfixes libXi
+            libXrandr libX11 libXcomposite libxshmfence libXtst libxcb
+          ]
+          );
         };
       });
     };
