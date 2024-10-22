4f50d7faf471a1200d6ed337850959b92f1993c5
jdev082 <jdev082@jdev.eu.org>
2024-10-22 16:12:12 -0500

    fix(nix): fix some library problems
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- flake.nix
+++ flake.nix
@@ -19,10 +19,13 @@
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.glib.out}/lib:${pkgs.nss.out}/lib:${pkgs.nspr.out}/lib:${pkgs.dbus.lib}/lib:${pkgs.atk.out}/lib:${pkgs.cups.lib}/lib:${pkgs.libdrm.out}/lib:${pkgs.gtk3.out}/lib:${pkgs.pango.out}/lib:${pkgs.cairo.out}/lib:${pkgs.xorg.libX11.out}/lib:${pkgs.xorg.libXcomposite.out}/lib:${pkgs.xorg.libXdamage.out}/lib:${pkgs.xorg.libXext.out}/lib:${pkgs.xorg.libXfixes.out}/lib:${pkgs.xorg.libXrandr.out}/lib
+          '';
           packages = (with pkgs;
             [
               nodejs python3 libcxx systemd libpulseaudio libdrm mesa stdenv.cc.cc
-              alsa-lib atk at-spi2-atk at-spi2-core cairo cups dbus expat fontconfig
+              alsa-lib atk at-spi2-atk at-spi2-core cairo cups dbus dbus-glib expat fontconfig
               freetype gdk-pixbuf glib glibc gtk3 libnotify libuuid nspr nss pango systemd
               libappindicator-gtk3 libdbusmenu libxkbcommon zlib
             ]
